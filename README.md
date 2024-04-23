70420505efec3c4e42ba46dc059350801330fc64
Xiaoyu Guo <biggates2010@gmail.com>
2024-04-23 16:34:00 +0800

    feat: 通过 hooks 精简不需要生成的文件和文件夹

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cookiecutter-python-project
 cookiecutter template for Python projects
+
+## 用法
+
+### 新建项目
+
+```bash
+$ pipx run cookiecutter gh:EaphoneTech/cookiecutter-python-project
+```
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,25 @@
+import shutil
+from pathlib import Path
+
+# 此处由 cookiecutter 由各个条件传入要删除的文件或文件夹
+__TO_DELETE_FILES = [
+    "{% if not cookiecutter.include_sphinx %}docs{% endif %}",
+    "{% if not cookiecutter.include_pytest %}tests{% endif %}",
+    "{% if not cookiecutter.enable_pre_commit %}.pre-commit-config.yaml{% endif %}",
+    "{% if not cookiecutter.include_git_cliff %}cliff.toml{% endif %}",
+    "{% if not cookiecutter.setup_ci %}.jenkins{% endif %}",
+]
+
+if __name__ == "__main__":
+    for file in __TO_DELETE_FILES:
+        # 跳过未生效的部分
+        if len(file) == 0:
+            continue
+
+        p = Path(file)
+        if p.is_file():
+            print("将要删除文件: ", p.absolute())
+            p.unlink(missing_ok=True)
+        elif p.is_dir():
+            print("将要删除文件夹: ", p.absolute())
+            shutil.rmtree(p, ignore_errors=True)
